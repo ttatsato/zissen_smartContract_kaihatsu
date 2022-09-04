af6810e31cd10fcb07e04f9c5a1cf28ec17513f9
ttatsato <t-satoh@alby.co.jp>
2022-09-04 14:25:42 +0900

    add: greet関数を追加

--- test/greeter.test.js
+++ test/greeter.test.js
@@ -6,3 +6,12 @@ contract("Greeter", () => {
         assert(greeter, "contract was not deployed");
     });
 });
+
+describe("Greet()", () => {
+    it("returns, 'Hello World", async() => {
+        const greeter = await GreeterContract.deployed();
+        const expected = "Hello World"
+        const actual = await greeter.greet();
+        assert.equal(actual, expected, "greeted with 'Hello world!'")
+    })
+})
