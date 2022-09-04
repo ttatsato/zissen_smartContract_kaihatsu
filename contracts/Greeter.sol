51dd4cd7ca2aa528d4a2a8a670365464da90d85c
ttatsato <t-satoh@alby.co.jp>
2022-09-04 16:07:25 +0900

    add: ownable modifierを追加

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,11 +4,29 @@ contract Greeter {
 
     string _greeting = "Hello World";
 
+    address _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require (
+            msg.sender == _owner,
+            "Ownable :caller is not the owner"
+        );
+        _;
+    }
+
     function greet() external view returns (string memory) {
         return _greeting;
     }
 
-    function setGreeting(string calldata greeting) external{
+    function setGreeting(string calldata greeting) external onlyOwner{
         _greeting = greeting;
+    }
+
+    function owner () public view returns(address) {
+        return _owner;
     }
 }
--- test/greeter.test.js
+++ test/greeter.test.js
@@ -1,21 +1,29 @@
 const GreeterContract = artifacts.require("Greeter");
 
-contract("Greeter", () => {
+contract("Greeter", (accounts) => {
     it("has been deployed successfully", async () => {
         const greeter = await GreeterContract.deployed();
         assert(greeter, "contract was not deployed");
     });
-});
 
-describe("Greet()", () => {
-    it("returns, 'Hello World", async() => {
-        const greeter = await GreeterContract.deployed();
-        const expected = "Hello World"
-        const actual = await greeter.greet();
-        assert.equal(actual, expected, "greeted with 'Hello world!'")
+
+    describe("Greet()", () => {
+        it("returns, 'Hello World", async() => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello World"
+            const actual = await greeter.greet();
+            assert.equal(actual, expected, "greeted with 'Hello world!'")
+        })
     })
-})
-
+    describe("owner()", () => {
+        it("matches the address that originarlly deployed the contract", async() => {
+            const greeter = await GreeterContract.deployed();
+            const owner = await greeter.owner();
+            const expect = accounts[0];
+            assert.equal(owner, expect, "matches the address used to deployed the contract");
+        })
+    })
+});
 contract("Greeter: update greeting", () => {
     describe("SetGreeting(string)", () => {
        it("sets greeting to passed in string", async () => {
